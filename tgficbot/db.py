eef1584929d370f5695169d71f2a41c67d1cbd6a
genelocated <genelocated@foxmail.com>
2019-08-13 11:55:02 +0800

    + init + find

--- tgficbot/db.py
+++ tgficbot/db.py
@@ -1,11 +1,64 @@
 import sqlite3
 import os
+from telethon.tl import types
 
 dbpath = os.path.expanduser('~/.cache/tgficbot.db')
 
 conn = sqlite3.connect(dbpath)
 cursor = conn.cursor()
 
-cursor.execute(
-    "CREATE TABLE IF NOT EXISTS channels (id INTERGER PRIMARY KEY, name varchar(101));"
-)
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS channels (
+        channel_id INTEGER PRIMARY KEY NOT NULL,
+        username TEXT,
+        title TEXT NOT NULL
+    );
+""")
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS messages (
+        rowid INTEGER PRIMARY KEY AUTOINCREMENT,
+        message_id INTEGER NOT NULL,
+        channel_id INTEGER NOT NULL,
+        content TEXT
+    );
+""")
+
+
+def check_channel_exist(full_channel: types.ChannelFull):
+    channel = full_channel.chats[0]
+    cursor.execute('SELECT * FROM channels WHERE channel_id = ?',
+                   (channel.id, ))
+    if cursor.fetchone() is not None:
+        return
+
+
+def save_channel(full_channel: types.ChannelFull):
+    channel = full_channel.chats[0]
+    cursor.execute(
+        'INSERT INTO channels (channel_id, username, title) VALUES (?, ?, ?)',
+        (channel.id, channel.username, channel.title))
+
+
+def save_message(message: types.Message):
+    if not isinstance(message, types.Message):
+        return
+    if message.message is None:
+        return
+    cursor.execute(
+        'INSERT INTO messages (message_id, channel_id, content) VALUES (?, ?, ?)',
+        (message.id, message.to_id.channel_id, message.message))
+
+
+def find_in_messages(channel_id: int, pattern: str):
+    cursor.execute('SELECT * FROM messages WHERE channel_id = ?',
+                   (channel_id, ))
+    messages = cursor.fetchall()
+    def filter_none(m):
+        if m[3] is None:
+            return False
+        return True
+    messages = filter(filter_none, messages)
+    matched_messages = [m for m in messages if m[3].find(pattern) != -1]
+    message_ids = [m[1] for m in matched_messages]
+    return message_ids
+
--- tgficbot/main.py
+++ tgficbot/main.py
@@ -3,6 +3,7 @@ from telethon.tl import types, functions
 import os
 import configparser
 import logging
+from tgficbot import db
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -25,22 +26,35 @@ async def start(event):
 @bot.on(events.NewMessage(pattern='/find'))
 async def find(event):
     args = event.raw_text.split()
+    if len(args) == 1:
+        await event.respond('Usage: `/find channel_name pattern`')
+
+    channel_name = args[1]
+    pattern = ' '.join(args[2:])
     chat = await event.get_chat()
     if isinstance(chat, types.User):
-        for text in args[1:]:
-            await event.respond(text)
-    elif isinstance(chat, types.Channel):
-        for text in args[2:]:
-            await event.respond(text)
+        channel = await bot.get_input_entity(channel_name)
+        found_message_ids = db.find_in_messages(channel.channel_id, pattern)
+        for message_id in found_message_ids:
+            await event.respond(f'https://t.me/{channel_name}/{message_id}')
 
 
-@bot.on(events.NewMessage(pattern='/get'))
+@bot.on(events.NewMessage(pattern='/init'))
 async def get_history(event):
-    channel = await bot.get_input_entity('genelocated')
-    full_channel = await bot(functions.channels.GetFullChannelRequest(channel=channel))
+    args = event.raw_text.split()
+    if len(args) != 2:
+        await event.respond('Usage: `/init channel_name`')
+        return
+
+    channel_name = args[1]
+    channel = await bot.get_input_entity(channel_name)
+    full_channel = await bot(
+        functions.channels.GetFullChannelRequest(channel=channel))
+    db.save_channel(full_channel)
     for i in range(full_channel.full_chat.read_inbox_max_id):
         message = await bot.get_messages(channel, ids=i)
-        print(message)
+        db.save_message(message)
+        db.conn.commit()
 
 
 bot.run_until_disconnected()
